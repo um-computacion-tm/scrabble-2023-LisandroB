b0ebfa370e3d0abb784dfa28c1124e526f276dfc
lisandrob <brasolinlisandro0@gmail.com>
2023-08-23 09:59:27 -0300

    Integrados los tests de board/cell/player/scrabble mas sus modelos

---
+++ game/cell.py
@@ -0,0 +1,18 @@
+from game.models import Tile
+
+class Cell:
+    def __init__(self, multiplier, multiplier_type):
+        self.multiplier = multiplier
+        self.multiplier_type = multiplier_type
+        self.letter = None
+
+    def add_letter(self, letter:Tile):
+        self.letter = letter
+
+    def calculate_value(self):
+        if self.letter is None:
+            return 0
+        if self.multiplier_type == 'letter':
+            return self.letter.value * self.multiplier
+        else:
+            return self.letter.value
---
+++ game/scrabble.py
@@ -0,0 +1,12 @@
+from game.board import Board
+from game.player import Player
+from game.models import BagTiles
+
+
+class ScrabbleGame:
+    def __init__(self, players_count):
+        self.board = Board()
+        self.bag_tiles = BagTiles()
+        self.players = []
+        for _ in range(players_count):
+            self.players.append(Player())
